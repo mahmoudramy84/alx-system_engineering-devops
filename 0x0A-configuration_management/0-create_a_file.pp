905ae0380b933c52f74d3c066a0dd36df6805f85
Mahmoud Elbehery <38046820+mahmoudramy84@users.noreply.github.com>
2024-01-07 07:08:42 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# This manifest creates a file at /tmp
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/README.md
+++
@@ -1,1 +0,0 @@
-0x0A-configuration_management tasks directory solutions For ALX software enginnering program
